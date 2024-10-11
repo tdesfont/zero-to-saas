ad913cf0997157f39cba060a7dfd7b9573fa7c16
Thibault Desfontaines <desfontainesthibault@gmail.com>
2024-10-11 11:34:18 +0200

    Add axios call to create an event

---
+++ mobile-app-simple-tasks-manager/components/tasksScheduler/TaskCreator.tsx
@@ -0,0 +1,86 @@
+import React from 'react';
+import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
+import axios from 'axios';
+
+const TaskCreator: React.FC = () => {
+
+    const getRandomId = () => { 
+        /* Generate a random resource id for the event as the id is a primary key and should be unique.
+        */
+        const id = "event.id." + Math.random().toString(16).slice(2);
+        return id;
+    }
+
+    let task = { 
+        "eventid": 'sample-event-id', // Placeholder for the eventId (Will be set in the call directly)
+        "title": "Deploying Cloud backend for Task Scheduler application",
+        "description": "Need to create a backend (Likely in GCP) in order to support task creation, storage and retrieval.", 
+        "starttimestamp": "Fri, 11 Oct 2024 11:32:00 GMT", 
+        "endtimestamp": "Fri, 11 Oct 2024 14:24:00 GMT", 
+        "status": "To be done", 
+    };
+    const sendTasksToServer = async () => {
+        try {
+            task.eventid = getRandomId();
+            const response = await axios.post('http://192.168.43.62:5000/create_task', task, {
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+            });
+            console.log('Tasks sent successfully:', response.data);
+        } catch (error) {
+            console.error('Error sending tasks:', error);
+        }
+    };
+
+
+    return (
+        <View style={styles.container}>
+            <View style={styles.tilesContainer}>
+                <TouchableOpacity
+                    onPress={() => sendTasksToServer()}
+                >
+                    <Text style={styles.tileText}>Create new event</Text>
+                </TouchableOpacity>
+            </View>
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        padding: 20,
+    },
+    tilesContainer: {
+        flexDirection: 'row',
+        flexWrap: 'wrap',
+        justifyContent: 'center',
+    },
+    tile: {
+        width: 80,
+        height: 80,
+        margin: 10,
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderRadius: 10,
+    },
+    tileText: {
+        color: 'black',
+        fontSize: 18,
+    },
+    button: {
+        marginTop: 20,
+        padding: 10,
+        backgroundColor: 'black',
+        borderRadius: 5,
+    },
+    buttonText: {
+        color: 'white',
+        fontSize: 16,
+    },
+});
+
+export default TaskCreator;
